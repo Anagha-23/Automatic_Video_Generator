9fc56f959e0949597fc86e52dae794a238f788e5
Anagha-23 <117373295+Anagha-23@users.noreply.github.com>
2024-06-21 18:52:39 +0530

    Create voice.py

---
+++ voice.py
@@ -0,0 +1,28 @@
+import requests
+import os
+import time
+
+def generate_voiceover(story, save_file=False):
+    headers = {
+        "xi-api-key": os.getenv("ELEVENLABS_API_KEY"),
+        "Content-Type": "application/json",
+        "accept": "audio/mpeg"
+    }
+    data = {
+        "text": story + "..Comment with your favorite fact...", 
+        "voice_settings": {"stability": 0.3, "similarity_boost": 0.3}
+    }
+    response = requests.post("https://api.elevenlabs.io/v1/text-to-speech/AZnzlk1XvdvUeBnXmlld", headers=headers, json=data)
+    if response.status_code == 200:
+        if save_file:
+            with open("file.mp3", "wb") as f:
+                f.write(response.content)
+        return response.content
+    else:
+        print(f"Error while generating voiceover with status code {response.status_code}")
+        return None
+
+def save_voiceover(voiceover_content, timestamp):
+    voiceover_filename = f"voiceover_{timestamp}.mp3"
+    with open(voiceover_filename, "wb") as f:
+        f.write(voiceover_content)
